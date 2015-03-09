7617700e6754f0d3d137eefe6d6ba08a098f6fa7
Steven Kutsch <skutsch40@engr-134-25.eastern.ewu.edu>
2015-03-09 13:34:25 -0700

    Version 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$('body').jQlouds({
+  maxClouds: 10
+});
+
+$('body').jQlouds({
+  src: 'img/new-cloud.png',
+});
+
+
+
+$('body').jQlouds({
+  maxWidth: 300
+});
+
+$('body').jQlouds({
+  minWidth: 300
+});
+
+$('body').jQlouds({
+  wind: true
+});
